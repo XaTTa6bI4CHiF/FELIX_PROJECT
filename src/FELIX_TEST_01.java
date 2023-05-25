53812a8b54ef97de44bff47fee376bc37862710d
XaTTa6bI4CHiF <134633829+XaTTa6bI4CHiF@users.noreply.github.com>
2023-05-25 22:50:49 +0300

    TEST

--- src/FELIX_TEST_01.java
+++ src/FELIX_TEST_01.java
@@ -1,33 +1,4 @@
-/*
- * This file is part of the L2J Mobius project.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
-import org.l2jmobius.Config;
-import org.l2jmobius.commons.enums.ServerMode;
-
-public class FELIX_TEST_01 // Delete old files.DatabaseBackup
+public class FELIX_TEST_01 //DatabaseBackup
 {
     public static void performBackup()
     {
