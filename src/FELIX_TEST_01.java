4db0ed6388f3f21c31a1d5c9fae009f470ae6609
XaTTa6bI4CHiF <134633829+XaTTa6bI4CHiF@users.noreply.github.com>
2023-05-25 22:26:14 +0300

    TEST

---
+++ src/FELIX_TEST_01.java
@@ -0,0 +1,9 @@
+public class FELIX_TEST_01 {
+    public static void main() {
+        main(null);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
