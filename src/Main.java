600ea782f6f6899c60f90f5b85244b1c1f271d58
XaTTa6bI4CHiF <134633829+XaTTa6bI4CHiF@users.noreply.github.com>
2023-05-30 20:30:03 +0300

    TEST

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Стараюсь понять как это работает");
@@ -6,10 +8,22 @@ public class Main {
     byte byteVariable = 127;
 
     char charVariable = '%';
-    short osliki = 158;
+    short felix = 158;
     int intVariable = 3231312;
     long longVariable = 321321312L;
     float floatVariable = 312.32f;
     double doubleVariable = 31231231.67d;
 
+    Scanner scanner = new Scanner(System.in);
+    //System.out.println("Name");
+    //System.out.println(felix+3);
+    //String userName = myObj.nextLine();
+    //System.out.println("Username is: " + userName");
+    String vuvidNaEkran = "Veddit 1 chuslo";
+    System.out.println(vuvidNaEkran);
+    int a = scanner.nextInt();
+    System.out.println("Veddit 2 chuslo");
+    int b = scanner.nextInt();
+    System.out.println("Summa = ");
+    System.out.println(a+b);
 }
