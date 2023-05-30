7d1c7940530ee806359d9377567adcc9c77e89b7
XaTTa6bI4CHiF <134633829+XaTTa6bI4CHiF@users.noreply.github.com>
2023-05-30 20:59:06 +0300

    TEST

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Стараюсь понять как это работает");
-    boolean BooleanVariable = true;
-    byte byteVariable = 127;
-
-    char charVariable = '%';
-    short felix = 158;
-    int intVariable = 3231312;
-    long longVariable = 321321312L;
-    float floatVariable = 312.32f;
-    double doubleVariable = 31231231.67d;
-
     Scanner scanner = new Scanner(System.in);
     String vuvidNaEkran = "Veddit 1 chuslo";
     System.out.println(vuvidNaEkran);
